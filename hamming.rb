fd7db48fa1d7f0e7a1c7b2293f6db044191b29a1
Adams1991 <adams121@hotmail.co.uk>
2018-06-05 13:49:39 +0100

    changing range variable name for clarity

--- hamming.rb
+++ hamming.rb
@@ -3,8 +3,8 @@ class Hamming
 
 def Hamming.compute(thread1,thread2)
   thread_tally_match = 0
-  length = thread1.length
-  for chr in 0..length
+  range_maximum = thread1.length
+  for chr in 0..range_maximum
     if thread1[chr] != thread2[chr]
       thread_tally_match += 1
     end
