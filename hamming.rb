47e3b972ae50b32378b74ddf573c59048c13609a
Adams1991 <adams121@hotmail.co.uk>
2018-06-05 08:44:22 +0100

    set up enumerator that is failing. further work/study needs done

--- hamming.rb
+++ hamming.rb
@@ -2,8 +2,12 @@ class Hamming
 
 
 def Hamming.compute(thread1,thread2)
-  return 0 if thread1.length && thread2.length == 0
-  return 0 if thread1 == thread2
+  thread_tally_match = 0
+  thread1.each_char { |chr|
+    thread_tally_match += 1 if chr == thread2.each_char
+  }
+  return thread_tally_match
 end
+
 
 end
