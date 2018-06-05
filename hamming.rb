d441cc70b6c1f5bb67d24940fc1437509c0967d0
Adams1991 <adams121@hotmail.co.uk>
2018-06-05 13:34:12 +0100

    13 of 15 tests passing. need confirmation that 0..length is doing what I think

--- hamming.rb
+++ hamming.rb
@@ -3,11 +3,13 @@ class Hamming
 
 def Hamming.compute(thread1,thread2)
   thread_tally_match = 0
-  thread1.each_char { |chr|
-    thread_tally_match += 1 if chr == thread2.each_char
-  }
+  length = thread1.length
+  for chr in 0..length
+    if thread1[chr] != thread2[chr]
+      thread_tally_match += 1
+    end
+  end
   return thread_tally_match
 end
-
 
 end
